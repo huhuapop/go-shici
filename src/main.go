e2f7ad43691cb20d67af76708e0aa35358eb0911
huhuafupop <huhuapop@outlook.com>
2021-06-18 09:42:59 -0400

    init

---
+++ src/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
+)
+
+func main()  {
+	authors := "https://so.gushiwen.cn/authors/"
+	res, err := http.Get(authors)
+	if err != nil{
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil{
+		fmt.Errorf("doc.err", err)
+	}
+	doc.Find(".sons").Find(".cont").Find("a").Each(func(i int, selection *goquery.Selection) {
+		authors :=selection.Text()
+		fmt.Printf("%d author=%s\n", i, authors)
+		link, _ := selection.Attr("href")
+		fmt.Printf("%d link=%s\n", i, link)
+	})
+
+
+	//fmt.Print("hello")
+	
+}
